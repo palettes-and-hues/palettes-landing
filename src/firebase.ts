138e10838b5d0d6ca791e38717428143259ec6be
Mayank Singh <mayank.singh081997@gmail.com>
2024-10-21 23:47:52 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: process.env.YOUR_API_KEY,
+  authDomain: process.env.YOUR_AUTH_DOMAIN,
+  projectId: process.env.YOUR_PROJECT_ID,
+  storageBucket: process.env.YOUR_STORAGE_BUCKET,
+  messagingSenderId: process.env.YOUR_MESSAGING_SENDER_ID,
+  appId: process.env.YOUR_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
